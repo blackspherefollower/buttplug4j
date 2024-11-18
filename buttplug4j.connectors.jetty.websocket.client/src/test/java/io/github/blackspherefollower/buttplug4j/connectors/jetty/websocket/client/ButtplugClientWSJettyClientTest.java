5d7738d01d0a54845c168740d9c69c32bb74952f
Fyustorm <fyustorm@gmail.com>
2024-11-18 17:50:13 +0100

    Fix nextMsgId and add timeout

--- buttplug4j.connectors.jetty.websocket.client/src/test/java/io/github/blackspherefollower/buttplug4j/connectors/jetty/websocket/client/ButtplugClientWSJettyClientTest.java
+++ buttplug4j.connectors.jetty.websocket.client/src/test/java/io/github/blackspherefollower/buttplug4j/connectors/jetty/websocket/client/ButtplugClientWSJettyClientTest.java
@@ -1,11 +1,14 @@
 package io.github.blackspherefollower.buttplug4j.connectors.jetty.websocket.client;
 
 import io.github.blackspherefollower.buttplug4j.client.ButtplugClientDevice;
+import io.github.blackspherefollower.buttplug4j.client.ButtplugDeviceException;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ButtplugClientWSJettyClientTest {
@@ -47,6 +50,31 @@ public class ButtplugClientWSJettyClientTest {
 
         Thread.sleep(1000);
         assertTrue(client.stopAllDevices());
+
+        client.disconnect();
+    }
+
+    @Disabled
+    @Test
+    public void TestBattery() throws Exception {
+        ButtplugClientWSClient client = new ButtplugClientWSClient("Java Test");
+        client.connect(new URI("ws://localhost:12345/buttplug"));
+        client.startScanning();
+
+        Thread.sleep(2000);
+        client.requestDeviceList();
+        for (ButtplugClientDevice dev : client.getDevices()) {
+            if (dev.hasBatterySensor()) {
+                long battery = dev.readBatteryLevel();
+                System.out.println("Battery is " + battery);
+                assertTrue(battery >= 0);
+                assertTrue(battery <= 100);
+            } else {
+                assertThrows(ButtplugDeviceException.class, () -> {
+                    long battery = dev.readBatteryLevel();
+                });
+            }
+        }
 
         client.disconnect();
     }
